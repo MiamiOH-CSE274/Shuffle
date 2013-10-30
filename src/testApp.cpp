b03bde80431849737130fac7db1d6f0d9d200d51
Mingwei Zhong <JohnneyMing@users.noreply.github.com>
2013-10-29 19:21:53 -0700

    Update Shuffle() method.

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,40 +1,31 @@
 #include "testApp.h"
-#include "ArrayQueue.h"
+#include "LinkedList.h"
+#include <ctime>
+#include <cstdlib>
 
-/* READ THIS FIRST!
- *
- * Before you do any development, you will need to tell the IDE where the openFrameworks libraries are installed.
- * Whenever you change computers you may have to re-do this step.
- *
- * Mac: 1) The easiest fix is to make sure your openFrameworks directories are 3 levels above your project directory.
- *         For example, on my system the .xcodeproj file is at 
- *         /Users/brinkmwj/Work/Teaching and learning/Courses/2013-2014_Fall/CSE274/git/Shuffle/Shuffle.xcodeproj 
- *         ... my openFrameworks libraries are at
- *         /Users/brinkmwj/Work/Teaching and learning/Courses/2013-2014_Fall/libs
- *
- */
 
 //How many experiments should we do between each re-draw of the screen?
-// Bigger numbers make the program much faster
+//Bigger numbers make the program much faster
 #define EXPS_PER_UPDATE 2
+
 //How many times should I call shuffle before measuring the result?
-#define SHUFFLES_PER_EXP 1
+#define SHUFFLES_PER_EXP 30
 
 //--------------------------------------------------------------
 void testApp::setup(){
     ofBackground(255);
-    
+
     //old OF default is 96 - but this results in fonts looking larger than in other programs.
 	ofTrueTypeFont::setGlobalDpi(72);
-    
+
     arial24.loadFont("arial.ttf",24);
-    
+
     for(int i=0;i<52*52;i++){
         shuffleCounts[i] = 0;
         randCounts[i] = 0;
     }
     experimentsDone = 0;
-    
+
     std::srand ( unsigned ( std::time(0) ) );
 }
 
@@ -53,7 +44,7 @@ void testApp::update(){
 void testApp::drawMatrix(float x, float y, float width, float height, double matrix[], double min, double max){
     float sqWidth = width/52.0;
     float sqHeight = height/52.0;
-    
+
     for(int i=0;i<52;i++){
         for(int j=0;j<52;j++){
             //j corresponds to x, i corresponds to y
@@ -75,7 +66,7 @@ void testApp::draw(){
     //Draw the two matrices
     drawMatrix(5,5+24+5+24+5,260,260,randCounts, 0, experimentsDone/26);
     drawMatrix(260+10,5+24+5+24+5,260,260,shuffleCounts,0,experimentsDone/26);
-    
+
     //Draw info about how long we have been running
     //Draw text in black
     ofSetColor(0);
@@ -84,31 +75,31 @@ void testApp::draw(){
     double strWidth = arial24.stringWidth(stringBuilder.str());
     double strX = (ofGetWidth() - strWidth)/2;
     arial24.drawString(stringBuilder.str(), strX, 260+5+24+5+24+24+5);
-    
+
     //Clear out the string buffer for re-use
     stringBuilder.clear();
     stringBuilder.str("");
-    
+
     //Calculate the p value for true random, and print it
     stringBuilder << "p < " << pValue(randCounts) << flush;
     strWidth = arial24.stringWidth(stringBuilder.str());
     strX = 5 + (260 - strWidth)/2;
     arial24.drawString(stringBuilder.str(), strX, 130+5+24+5+24+24+5);
-    
+
     //Clear out the string buffer for re-use
     stringBuilder.clear();
     stringBuilder.str("");
-    
+
     //Calculate the p value for shuffle, and print it
     stringBuilder << "p < " << pValue(shuffleCounts) << flush;
     strWidth = arial24.stringWidth(stringBuilder.str());
     strX = 10 + 260 + (260 - strWidth)/2;
     arial24.drawString(stringBuilder.str(), strX, 130+5+24+5+24+24+5);
-    
+
     //Clear out the string buffer for re-use
     stringBuilder.clear();
     stringBuilder.str("");
-    
+
     //Calculate the p value for shuffle, and print it
     stringBuilder << "If shuffle is truly random, what is chance\nof this outcome? (small is bad)" << flush;
     strWidth = arial24.stringWidth(stringBuilder.str());
@@ -119,17 +110,19 @@ void testApp::draw(){
 /*
  * Use this as a comparison for your shuffle algorithm. This
  * is an implementation of the Fisher-Yates Shuffle
- * (http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle )
+ * (http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle)
  * which is generally considered to be the right way to generate
  * a uniformly random permuation.
  */
-void randomize(unsigned int cards[], unsigned int len){
-    for(int i=1; i<len; i++){
+void randomize(unsigned int cards[], unsigned int len)
+{
+        for(int i=1; i<len; i++)
+        {
         //We are going to swap item len-i with a random
-        // item that comes before it (or itself)
+        //item that comes before it (or itself)
         unsigned int t = cards[len-i];
         unsigned int which = rand()%(len-i+1);
-        
+
         cards[len-i] = cards[which];
         cards[which] = t;
     }
@@ -141,20 +134,70 @@ void testApp::doRandExperiment(){
     for(int i=0;i<52;i++){
         cards[i] = i;
     }
-    
+
     //randomly scramble it using Fisher-Yates shuffle
     randomize(cards,52);
-    
+
     for(int i=0;i<52;i++){
         randCounts[i*52 + cards[i]] += 1.0;
     }
 }
 
-void shuffle(unsigned int cards[], unsigned int len){
-    //TODO Replace this with your own function that simulates the shuffling of a deck
-    // of cards
-    randomize(cards,len);
-}
+
+
+
+void shuffle(unsigned int cards[], unsigned int len)
+{
+        //Creating a list to store whole deck of card.
+        LinkedList<int> deck;
+
+
+        for(int i = 0;i<len;i++)
+        {
+          deck.add(i,cards[i]);
+
+        }
+
+        //store the cards after shuffling.
+        LinkedList<int> newDeck;
+
+        //Add the first card into newDeck.
+        unsigned int sacrifice = cards[0];
+
+        //add sacrifice as first node in newDeck.
+        newDeck.add(0,sacrifice);
+
+
+        for(int i = 0;i<len;i++)
+        {
+            if(i == 0)
+            {
+               //Move a card from deck and append it behind sacrifice.
+               deck.splice(0,1,newDeck,0);
+
+               //Delete sacrifice.
+               newDeck.remove(0);
+            }
+
+            else
+            {
+                int randomIndex = rand() % i;
+
+                //Insert a card into random position into newDeck.
+                deck.splice(0,1,newDeck,randomIndex);
+            }
+        }
+
+          for(int i = 0;i<len;i++)
+          {
+              cards[i] = newDeck.get(i);
+
+          }
+    }
+
+
+
+
 
 void testApp::doShuffleExperiment(int numShuffles){
     //Initialize a deck of cards
@@ -162,12 +205,12 @@ void testApp::doShuffleExperiment(int numShuffles){
     for(int i=0;i<52;i++){
         cards[i] = i;
     }
-    
+
     //randomly scramble it using shuffle
     for(int i=0; i<SHUFFLES_PER_EXP;i++){
         shuffle(cards,52);
     }
-    
+
     for(int i=0;i<52;i++){
         shuffleCounts[i*52 + cards[i]] += 1.0;
     }
@@ -183,7 +226,7 @@ std::string testApp::pValue(double matrix[]){
     // a statistical test to prove a shuffle algorithm is truly random.
     // Instead, you would have to really prove it using probability theory.
     // This is just a quick and dirty test that is helpful for debugging.
-    
+
     double chiSq = 0.0;
     double expected = ((double)experimentsDone)/52;
     for(int i=0;i<52;i++){
@@ -191,7 +234,7 @@ std::string testApp::pValue(double matrix[]){
         chiSq += (observed-expected)*(observed-expected);
     }
     chiSq = chiSq/expected;
-    
+
     //Critical values for 51 degrees of freedom
     double chi[] = {0.0,42.36,47.84,52.92,59.25,64.3,68.67,77.39,80.75};
     std::string p[] = {"100%","80%","60%","40%","20%","10%","5%","1%","0.5%"};
@@ -200,7 +243,9 @@ std::string testApp::pValue(double matrix[]){
     while(index < 8){
         if(chiSq >= chi[index+1]){
             index++;
-        } else {
+        }
+        else
+        {
             break;
         }
     }
